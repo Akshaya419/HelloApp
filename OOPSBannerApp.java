a41eb7405bd5a3a3533500a16b48eec2836b78da
Akshaya419 <ar7002@srmist.edu.in>
2026-03-02 13:12:33 +0530

    UC2

---
+++ OOPSBannerApp.java
@@ -0,0 +1,25 @@
+package STEPCLASS;
+
+/**
+ * OOPSBannerApp UC2 – OOPS asterisk print 7 line
+ *
+ * * @author Akshaya419
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("  **    " +"  **  "+"  **   "+"   ***  ");
+        System.out.println(" *   *   " +" *   * "+"  *   *  "+" *    * ");
+        System.out.println("*     *  " +"*     *"+"  *   *  "+" **       ");
+        System.out.println("*     *  " +"*     *"+"  **   "+"  **  ");
+        System.out.println("*     *  " +"*     *"+"  *       "+"       * ");
+        System.out.println(" *   *   " +" *   * "+"  *       "+" *     **");
+        System.out.println("  **    " +"  **  "+"  *       "+"  ***  ");
+        
+
+    }
+}
+
+
