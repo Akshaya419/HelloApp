431a8e5386fdeaf7f269c797cfaf8acf3c19f17e
Akshaya419 <ar7002@srmist.edu.in>
2026-03-02 13:35:35 +0530

    UC3:

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,22 @@
 package STEPCLASS;
 
 /**
- * OOPSBannerApp UC2 – OOPS asterisk print 7 line
- *
- * * @author Akshaya419
- * @version 1.0
+ * OOPSBannerApp UC3 – Render OOPS as Banner using String.join()
+ * Optimized for alignment and readability.
+ * @author Akshaya419
+ * @version 3.1
  */
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("  **    " +"  **  "+"  **   "+"   ***  ");
-        System.out.println(" *   *   " +" *   * "+"  *   *  "+" *    * ");
-        System.out.println("*     *  " +"*     *"+"  *   *  "+" **       ");
-        System.out.println("*     *  " +"*     *"+"  **   "+"  **  ");
-        System.out.println("*     *  " +"*     *"+"  *       "+"       * ");
-        System.out.println(" *   *   " +" *   * "+"  *       "+" *     **");
-        System.out.println("  **    " +"  **  "+"  *       "+"  ***  ");
-        
+        // We use a delimiter of "  " (two spaces) to separate the letters clearly.
+        String sep = "  ";
 
+        System.out.println(String.join(sep, "  *** ",  "  *** ",   " ***** ", "  **** "));
+        System.out.println(String.join(sep, " *   * ", " *   * ", " *    *", " * "));
+        System.out.println(String.join(sep, " *   * ", " *   * ", " *    *", " * "));
+        System.out.println(String.join(sep, " *   * ", " *   * ", " ***** ", "  *** "));
+        System.out.println(String.join(sep, " *   * ", " *   * ", " *     ", "     * "));
+        System.out.println(String.join(sep, " *   * ", " *   * ", " *     ", "     * "));
+        System.out.println(String.join(sep, "  *** ",  "  ***   ", " *     "," **** "));
     }
 }
-
-
